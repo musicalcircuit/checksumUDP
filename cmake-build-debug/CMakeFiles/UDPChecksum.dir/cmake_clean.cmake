7a22fa0d7db3eb5e1f913b1b4d52bc0180eb72fb
musicalcircuits <aslangeb@ncsu.edu>
2020-03-21 01:56:02 -0400

    second

---
+++ cmake-build-debug/CMakeFiles/UDPChecksum.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UDPChecksum.dir/main.cpp.o"
+  "UDPChecksum"
+  "UDPChecksum.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UDPChecksum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,7 @@
+// Verify UDP checksum by creating 16 bit hex object list
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
